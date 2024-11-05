3ce57c10b1af53a7a2f5cc294a77292d718554a9
valeriabukova <valeriasergeevna688@gmail.com>
2024-11-05 22:15:35 +0200

    Initial Commit

---
+++ homework binarnee tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework binarnee tree
+//
+//  Created by Valeria  Bukova on 05.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
